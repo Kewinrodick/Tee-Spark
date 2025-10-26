d49e394c5d13402789f0253650333b1087c2252b
Kewin Rodick <rodickkewin@gmail.com>
2025-10-26 08:52:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4083816315-c54e9",
+  "appId": "1:252121313529:web:89369b35a0e8ba990e3228",
+  "apiKey": "AIzaSyD5aYDCrQ_w0jY-FRg2vPISeBXFx_HiqjU",
+  "authDomain": "studio-4083816315-c54e9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "252121313529"
+};
